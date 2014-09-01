237450f2596be10db1c9b6e15248066877a924f3
jirachot <jirachot23@gmail.com>
2014-09-01 17:10:45 +0700

    Resume First

---
+++ gen/app/lab/profile495/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.lab.profile495;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/app/lab/profile495/ImageActivity.java
@@ -0,0 +1,14 @@
+package app.lab.profile495;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_jirachot);
+	}
+}
